45fa441b090ada592e389ba0ae55447065a8f0ca
Ousseynou DIAGNE <onzou10@gmail.com>
2021-11-04 01:34:40 +0000

    ok! updated!

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import donation  from './donation';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Modal, Button } from "react-bootstrap";
+import Header from './header/header';
 
 toast.configure();
 
@@ -118,7 +119,9 @@ class App extends Component
       
       <div className="container">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>
-
+      <Header>
+        
+      </Header>
       {
         this.isLoading? <p>Chargement...</p> : 
         <div className="container">
---
+++ src/header/header.js
@@ -0,0 +1,21 @@
+import './header.css';
+
+const Header = () => 
+{
+
+  return (
+    <header className="header">
+        <nav>
+            <div className="logo">
+            Donation
+            </div>
+            <div className="connection">
+            {/* <div className="link one">Créer un post</div> */}
+            </div>
+            
+        </nav>
+    </header>
+  );
+};
+
+export default Header;
